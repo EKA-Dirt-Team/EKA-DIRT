3170d243a112e1ba6c87b50fe022f6fe41c8c9e1
Reinis Mednis <mednisreinis@gmail.com>
2023-05-17 01:46:23 +0300

    Commit Test
    This is a test commit!

--- Assets/Scripts/Buttons.cs
+++ Assets/Scripts/Buttons.cs
@@ -3,16 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-
+// Test Commit
 public class Buttons : MonoBehaviour
 {
-   public void PlayGame()
-   {
-	SceneManager.LoadScene(2);
-	}
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(2);
+    }
 
-	public void Back()
-	{
-		SceneManager.LoadScene(1);
-	}
+    public void Back()
+    {
+        SceneManager.LoadScene(1);
+    }
 }
