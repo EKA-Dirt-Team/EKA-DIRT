947eb8b7758b0b09caeb40bd52040982d421d788
dinccc <tigerlover542@gmail.com>
2023-05-19 01:06:58 +0300

    pickup + counter

---
+++ Assets/Scripts/DestroyLeafV2.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DestroyLeafV2 : MonoBehaviour
+{
+    private float leaves = 0;
+    public TextMeshProUGUI textLeaves;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Pickup"))
+        {
+            Debug.Log("boop");
+            leaves++;
+            textLeaves.text = leaves.ToString();
+            Destroy(collision.gameObject);
+        }
+    }
+}
