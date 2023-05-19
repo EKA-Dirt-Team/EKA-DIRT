9b01409ff4ac50e14a3c59ec071576c8a4c55ffc
Reinis Mednis <mednisreinis@gmail.com>
2023-05-19 21:08:19 +0300

    Added ESC support
    The menu can now be opened and closed using the ESC key

--- Assets/Scripts/MenuOptions.cs
+++ Assets/Scripts/MenuOptions.cs
@@ -3,14 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Experimental.GlobalIllumination;
 using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
 
 public class MenuOptions : MonoBehaviour
 {
     
     public GameObject menu;
     public GameObject gameUI;
+    public GameObject audioButton;
+    public Color audioDisabledColor;
+    public Color audioEnabledColor;
 
     private bool menu_visible = false;
+    private bool audio_state = true;
+    
+    private void Start()
+    {
+        menu.SetActive(menu_visible);
+        gameUI.SetActive(!menu_visible);
+        menu_visible = menu.activeSelf;
+        audio_state = AudioListener.pause;
+    }
+    
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            ShowMenu();
+        }
+    }
     
     public void ShowMenu()
     {
@@ -36,6 +58,19 @@ public class MenuOptions : MonoBehaviour
     public void audioToggle()
     {
         Debug.Log("Audio Toggled");
-        AudioListener.pause = !AudioListener.pause;
+        audio_state = !audio_state;
+        AudioListener.pause = audio_state;
+        
+        if (audio_state)
+        {
+            Debug.Log("Audio On");
+            audioButton.GetComponent<Image>().color = audioEnabledColor;
+        }
+        else
+        {
+            Debug.Log("Audio Off");
+            audioButton.GetComponent<Image>().color = audioDisabledColor;
+        }
+        
     }
 }
