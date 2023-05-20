c17f4e61c7a599bbf41bfca9685eff2a258fa5b3
Reinis Mednis <mednisreinis@gmail.com>
2023-05-20 16:56:52 +0300

    Made the background material be set automagically
    Working with fully unlit materials in the editor was a but painfull, with this you can work on a fully unlit texture while still renering the fully lit version in the game itself :D

--- Assets/Scripts/ShowBackground.cs
+++ Assets/Scripts/ShowBackground.cs
@@ -6,9 +6,11 @@ public class ShowBackground : MonoBehaviour
 {
     
     public GameObject Background;
+    public Material BackgroundMaterial;
     // Start is called before the first frame update
     void Start()
     {
         Background.SetActive(true);
+        Background.GetComponent<SpriteRenderer>().material = BackgroundMaterial;
     }
 }
